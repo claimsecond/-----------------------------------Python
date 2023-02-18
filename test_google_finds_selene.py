74e1d022d600378f71a894670ab9c7f393bc0aea
claimsecond <claim.second@gmail.com>
2023-02-18 23:28:07 +0200

    tried to run FF and fall because of the res number

--- test_google_finds_selene.py
+++ test_google_finds_selene.py
@@ -1,8 +1,10 @@
 from selene.support.shared import browser
 from selene import *
 
-
-browser.config.hold_browser_open = True
+browser.config.hold_browser_open = False
+browser.config.browser_name = 'firefox'
 browser.open('https://google.com/ncr')
 browser.element('[name=q]').type('selene').press_enter()
-browser.element('[id=search]').should(have.text('User-oriented Web UI'))
+browser.element('[id=search]').should(
+    have.text('User-oriented Web UI browser tests in Python'))
+browser.all('#rso>div').should(have.size(12))
